7268152fb8ae4c540335bc755b940b97e16dae42
MeriSimonyan <60382275+MeriSimonyan@users.noreply.github.com>
2020-02-14 16:26:22 +0400

    Update 7.13.02.js

--- 7.13.02.js
+++ 7.13.02.js
@@ -1,11 +1,9 @@
 let triangle
 let rectangular
-let height
-let base
-let side1
-let side2
 letqa = prompt('Do you want to calculate the area of the triangle or rectangular ?') 
 if (qa = triangle) {
+let height
+let base
 let message1 = prompt('please enter height and base')
 if (message1 > 0) {
 alert(height * base / 2)
@@ -14,10 +12,11 @@ alert(height * base / 2)
 else if (qa = rectangular) {
 let message2 = prompt('please enter side1 and side2')
 if (message2 > 0){
+let side1
+let side2
 alert(side1 * side2)
   }
 } 
 else {
 alert('Please enter only positives')
 }
-//I don't understand why operator if is not working 
